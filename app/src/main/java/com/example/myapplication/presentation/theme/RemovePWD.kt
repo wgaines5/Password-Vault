febca8c6367d6ec98f3ccee901fff330794e6517
Wgaines5 <willie.8chd@gmail.com>
2024-09-09 16:05:19 -0400

    Remove Pwd page created not functional

---
+++ app/src/main/java/com/example/myapplication/presentation/theme/RemovePWD.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.presentation.theme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.myapplication.R
+
+class RemovePWD : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.removepw)
+    }
+}
