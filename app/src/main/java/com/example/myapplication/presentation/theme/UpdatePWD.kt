6655193d6107ef231d1476ebc1be0a64e06ff69c
Wgaines5 <willie.8chd@gmail.com>
2024-09-20 17:26:02 -0400

    All Update functions active

--- app/src/main/java/com/example/myapplication/presentation/theme/UpdatePWD.kt
+++ app/src/main/java/com/example/myapplication/presentation/theme/UpdatePWD.kt
@@ -1,12 +1,60 @@
 package com.example.myapplication.presentation.theme
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.myapplication.R
+import com.example.myapplication.presentation.MainScreen
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QueryDocumentSnapshot
 
 class UpdatePWD : ComponentActivity() {
+    private val dataB = FirebaseFirestore.getInstance()
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.updatepw)
+
+        val pwdListContainer = findViewById<LinearLayout>(R.id.pwdLContainer)
+        val cancelButton = findViewById<Button>(R.id.cancelB)
+
+        dataB.collection("Passwords")
+            .get()
+            .addOnSuccessListener {
+                documents -> for (document in documents){
+                    addPasswordB(document, pwdListContainer)
+                }
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "No Passwords Retrieved",Toast.LENGTH_SHORT).show()
+            }
+
+        cancelButton.setOnClickListener {
+            val intent = Intent(this, MainScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun addPasswordB(document: QueryDocumentSnapshot, container: LinearLayout){
+        val siteOrAppName = document.getString("siteOrAppName") ?: "Unknown"
+        val password = document.getString("password") ?: ""
+        val button = Button(this)
+        button.text = siteOrAppName
+
+        button.setOnClickListener {
+            val intent = Intent(this, UpwdForm::class.java).apply{
+                putExtra("docId", document.id)
+                putExtra("siteOrAppName", siteOrAppName)
+                putExtra("password", password)
+                putExtra("expirationDate", document.getDate("expirationDate"))
+            }
+            startActivity(intent)
+        }
+        container.addView(button)
     }
 }
